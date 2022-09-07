7289b59e99a306967de870e6e2fffd1b525eb38a
Alex <tanko77@rambler.ru>
2022-09-07 21:19:44 +0300

    палиндром

--- Program.cs
+++ Program.cs
@@ -3,29 +3,19 @@
 // 23432 -> да
 // 12821 -> да
 
-    //     Console.Write("Введите пятизначное число: ");
-    //     string? number = Console.ReadLine();
-    //  void CheckingNumber(string number)
-    //     {
-    //         if (number[0] == number[4] || number[1] == number[3])
-    //         {
-    //             Console.WriteLine($"Ваше число: {number} - палиндром.");
-    //         }
-    //         else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
-    //     }
-    //  if (number!.Length == 5)
-    //     {
-    //         CheckingNumber(number);
-    //     }
-    //     else Console.WriteLine($"Введи правильное число");
-    
-
-
+try
+   { 
     Console.Write("Введите пятизначное число: ");
     int number = Convert.ToInt32(Console.ReadLine());
- 
-    if (number/1000==(number%10)*10+(number/10)%10)
+    if (number.ToString().Length != 5)
+    Console.WriteLine("Введенное число не пятизначное! Введите пятизначное число!"); 
+    else if (number/1000==(number%10)*10+(number/10)%10)
         Console.WriteLine("палиндром."); 
     else 
         Console.WriteLine("НЕ палиндром.");
+    }
     // return;
+catch
+{
+    Console.WriteLine("Возникло исключение!");
+}
