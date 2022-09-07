7527638f66c55d90349808c64fe1c312d47e27c8
Alex <tanko77@rambler.ru>
2022-09-07 20:57:24 +0300

    д

--- Program.cs
+++ Program.cs
@@ -3,19 +3,29 @@
 // 23432 -> да
 // 12821 -> да
 
-        Console.Write("Введите пятизначное число: ");
-        string? number = Console.ReadLine();
-     void CheckingNumber(string number)
-        {
-            if (number[0] == number[4] || number[1] == number[3])
-            {
-                Console.WriteLine($"Ваше число: {number} - палиндром.");
-            }
-            else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
-        }
-     if (number!.Length == 5)
-        {
-            CheckingNumber(number);
-        }
-        else Console.WriteLine($"Введи правильное число");
+    //     Console.Write("Введите пятизначное число: ");
+    //     string? number = Console.ReadLine();
+    //  void CheckingNumber(string number)
+    //     {
+    //         if (number[0] == number[4] || number[1] == number[3])
+    //         {
+    //             Console.WriteLine($"Ваше число: {number} - палиндром.");
+    //         }
+    //         else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+    //     }
+    //  if (number!.Length == 5)
+    //     {
+    //         CheckingNumber(number);
+    //     }
+    //     else Console.WriteLine($"Введи правильное число");
     
+
+
+    Console.Write("Введите пятизначное число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+ 
+    if (number/1000==(number%10)*10+(number/10)%10)
+        Console.WriteLine("палиндром."); 
+    else 
+        Console.WriteLine("НЕ палиндром.");
+    // return;
