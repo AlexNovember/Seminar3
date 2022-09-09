dc9eafa76628af890b284034dc75377dd738b4bd
Alex <tanko77@rambler.ru>
2022-09-09 22:40:52 +0300

    4

--- Program.cs
+++ Program.cs
@@ -27,23 +27,59 @@
 
 
 
-int x1 = Coordinate("x", "A");
-int y1 = Coordinate("y", "A");
-int z1 = Coordinate("z", "A");
-int x2 = Coordinate("x", "B");
-int y2 = Coordinate("y", "B");
-int z2 = Coordinate("z", "B");
+// int x1 = Coordinate("x", "A");
+// int y1 = Coordinate("y", "A");
+// int z1 = Coordinate("z", "A");
+// int x2 = Coordinate("x", "B");
+// int y2 = Coordinate("y", "B");
+// int z2 = Coordinate("z", "B");
 
-int Coordinate(string coorName, string pointName)
+// int Coordinate(string coorName, string pointName)
+
+// {
+//     Console.Write($"Введите координату {coorName} точки {pointName}: ");
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// double Decision(double x1, double x2, double y1, double y2, double z1, double z2)
+// {
+//   return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
+// }
+
+// double segmentLength = Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2 );
+// Console.WriteLine($"Расстояние между точками = {segmentLength}");
+
+
+
+
+//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
 {
-    Console.Write($"Введите координату {coorName} точки {pointName}: ");
-    return Convert.ToInt32(Console.ReadLine());
+  int counter = 0;
+  int length = cube.Length;
+  while (counter < length)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
 }
-double Decision(double x1, double x2, double y1, double y2, double z1, double z2)
+
+void PrintArry(int[] coll)
 {
-  return Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
-}
-double segmentLength = Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2 );
-Console.WriteLine($"Длина отрезка = {segmentLength}");
+  int count = coll.Length;
+  int index = 1;
+  while(index < count)
+  {
+    Console.WriteLine(coll[index]+ " ");
+    index++;
+  }
+} 
 
-
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
